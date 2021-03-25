2d48c5f03a4a93e8a605e90b9574cde738a69797
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-03-25 14:15:10 +1300

    Suggested refactor

--- day-02/Main.elm
+++ day-02/Main.elm
@@ -24,95 +24,71 @@ type Msg
     = Change String
 
 
-isParenthesis : Char -> Bool
-isParenthesis c =
-    c == '(' || c == ')'
-
-
-charToParenthesis : Char -> Parenthesis
+charToParenthesis : Char -> Maybe Parenthesis
 charToParenthesis c =
-    if c == '(' then
-        Left
+    case c of
+        '(' ->
+            Just Left
 
-    else
-        Right
+        ')' ->
+            Just Right
 
-
-stringToListOfParenthsis : String -> List Parenthesis
-stringToListOfParenthsis str =
-    List.map charToParenthesis (String.toList (onlyParenthesis str))
-
-
-onlyParenthesis : String -> String
-onlyParenthesis str =
-    String.filter isParenthesis str
+        _ ->
+            Nothing
 
 
-checkParenthesis : Parenthesis -> Maybe Int -> Maybe Int
-checkParenthesis p count =
-    case p of
-        Left ->
-            Maybe.map ((+) 1) count
-
-        Right ->
-            case count of
-                Nothing ->
-                    Nothing
-
-                Just v ->
-                    if v == 0 then
-                        Nothing
-
-                    else
-                        Just (v - 1)
-
-
-isValid : Maybe Int -> Bool
-isValid count =
-    case count of
-        Nothing ->
-            False
-
-        Just v ->
-            v == 0
-
-
-computeValid : List Parenthesis -> Maybe Int
-computeValid p =
-    List.foldl checkParenthesis (Just 0) p
-
-
-boolToString : Bool -> String
-boolToString b =
-    case b of
-        True ->
-            "Valid"
-
-        False ->
-            "Invalid"
-
-
-wholeCheck : String -> String
-wholeCheck str =
+openParens : String -> Maybe Int
+openParens str =
     str
-        |> stringToListOfParenthsis
-        |> computeValid
-        |> isValid
-        |> boolToString
+        |> String.toList
+        |> List.filterMap charToParenthesis
+        |> List.foldl
+            (\p count ->
+                case p of
+                    Left ->
+                        count
+                            |> Maybe.map (\n -> n + 1)
+
+                    Right ->
+                        count
+                            |> Maybe.andThen
+                                (\n ->
+                                    if n == 0 then
+                                        Nothing
+
+                                    else
+                                        Just (n - 1)
+                                )
+            )
+            (Just 0)
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         Change str ->
-            { model | parenthesis = onlyParenthesis str }
+            { model | parenthesis = str }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ div [] [ text (wholeCheck model.parenthesis) ]
-        , input [ class "form-control", placeholder "type a series of ()", value model.parenthesis, onInput Change ] []
+        [ div []
+            [ text
+                (if openParens model.parenthesis == Just 0 then
+                    "Valid"
+
+                 else
+                    "Invalid"
+                )
+            ]
+        , input
+            [ class "form-control"
+            , placeholder "type a series of ()"
+            , value model.parenthesis
+            , onInput Change
+            ]
+            []
         ]
 
 
