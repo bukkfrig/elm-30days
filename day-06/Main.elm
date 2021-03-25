4c5cdaed057e31e10a9446cebb714b3ef20753cd
Jeremy Hunt <huntingfrogs@hotmail.com>
2021-03-26 12:06:41 +1300

    Suggestions for Day 06

--- day-06/Main.elm
+++ day-06/Main.elm
@@ -2,14 +2,199 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, table, td, text, tr)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (disabled, style)
 import Html.Events exposing (onClick)
-import Matrix
+import Matrix exposing (Matrix)
+import Time
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> ( initialModel, Cmd.none )
+        , view = view
+        , update = \msg model -> ( update msg model, Cmd.none )
+        , subscriptions =
+            \{ running } ->
+                if running then
+                    Time.every 500 Tick
+
+                else
+                    Sub.none
+        }
+
+
+
+-- Model
 
 
 type Living
     = Alive
     | Dead
+
+
+type alias Cell =
+    { st : Living
+    }
+
+
+type alias Model =
+    { running : Bool
+    , matrix : Matrix Cell
+    }
+
+
+initialModel : Model
+initialModel =
+    { running = False
+    , matrix =
+        Matrix.repeat ( 10, 10 )
+            (Cell Dead)
+    }
+
+
+
+-- Update
+
+
+type Msg
+    = Swap ( Int, Int )
+    | RequestedUpdateRunning Bool
+    | Tick Time.Posix
+
+
+update : Msg -> Model -> Model
+update msg ({ matrix } as model) =
+    case msg of
+        Swap ( x, y ) ->
+            { model
+                | matrix =
+                    matrix
+                        |> matrixUpdate ( x, y )
+                            (Maybe.map swap >> Maybe.withDefault (Cell Dead))
+            }
+
+        RequestedUpdateRunning isRunning ->
+            { model | running = isRunning }
+
+        Tick _ ->
+            { model | matrix = conway matrix }
+
+
+matrixUpdate : ( Int, Int ) -> (Maybe a -> a) -> Matrix a -> Matrix a
+matrixUpdate coord f matrix =
+    matrix
+        |> Matrix.set coord
+            (f (Matrix.get coord matrix))
+
+
+swap : Cell -> Cell
+swap c =
+    case c.st of
+        Dead ->
+            { c | st = Alive }
+
+        Alive ->
+            { c | st = Dead }
+
+
+conway : Matrix Cell -> Matrix Cell
+conway matrix =
+    matrix
+        |> Matrix.indexedMap
+            (\coord c ->
+                let
+                    livingNeighbours =
+                        numberLivingNeighbours coord matrix
+                in
+                case c.st of
+                    Dead ->
+                        if livingNeighbours == 3 then
+                            { st = Alive }
+
+                        else
+                            { st = Dead }
+
+                    Alive ->
+                        if livingNeighbours == 2 || livingNeighbours == 3 then
+                            { st = Alive }
+
+                        else
+                            { st = Dead }
+            )
+
+
+numberLivingNeighbours : ( Int, Int ) -> Matrix Cell -> Int
+numberLivingNeighbours ( x, y ) matrix =
+    matrix
+        |> Matrix.toIndexedList
+        |> List.filter
+            (\( ( otherX, otherY ), cell ) ->
+                let
+                    deltaX =
+                        x - otherX
+
+                    deltaY =
+                        y - otherY
+                in
+                (cell.st == Alive)
+                    && not (x == otherX && y == otherY)
+                    && (-1 <= deltaX && deltaX <= 1)
+                    && (-1 <= deltaY && deltaY <= 1)
+            )
+        |> List.length
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view { matrix, running } =
+    div []
+        [ matrixView
+            matrix
+        , button
+            [ onClick (RequestedUpdateRunning False)
+            , disabled (not running)
+            ]
+            [ text "Stop" ]
+        , button
+            [ onClick (RequestedUpdateRunning True)
+            , disabled running
+            ]
+            [ text "Start" ]
+        ]
+
+
+matrixView : Matrix Cell -> Html Msg
+matrixView matrix =
+    table []
+        (matrix
+            |> Matrix.toList
+            |> List.indexedMap
+                (\y row ->
+                    tr []
+                        (row
+                            |> List.indexedMap
+                                (\x cell ->
+                                    cellView ( x, y ) cell
+                                )
+                        )
+                )
+        )
+
+
+cellView : ( Int, Int ) -> Cell -> Html Msg
+cellView coord cell =
+    td
+        [ style "border" "1px black solid"
+        , style "width" "25px"
+        , style "height" "25px"
+        , style "background-color" (livingToColor cell.st)
+        , onClick (Swap coord)
+        ]
+        []
 
 
 livingToColor : Living -> String
@@ -20,158 +205,3 @@ livingToColor l =
 
         Alive ->
             "black"
-
-type alias Cell =
-    { st : Living
-    }
-
-type alias Model = 
-    { matrix : Matrix.Matrix Cell
-    , mode : TouchMode
-    } 
-    
-type TouchMode 
-    = Set
-    | Update
-
-initialModel : Model
-initialModel =
-    { matrix = Matrix.repeat ( 10, 10 ) { st = Dead }
-    , mode = Set
-    }
-
-type Msg
-    = Swap ( Int, Int )
-    | ChMode TouchMode
-
-swap : Maybe Cell -> Cell
-swap mc =
-    case mc of
-        Nothing ->
-            { st = Dead }
-
-        Just c ->
-            case c.st of
-                Dead ->
-                    { st = Alive }
-
-                Alive ->
-                    { st = Dead }
-
- 
-indexedFilter :  ((Int, a) -> Bool) -> List a -> List a
-indexedFilter f l =
-   l
-   |> (List.indexedMap Tuple.pair)
-   |> (List.filter f)
-   |> (List.map Tuple.second)
-   
-
-around : Int -> (Int,a) -> Bool
-around p (i,_) =
-    if p-i < -1 then
-        False
-    else if p-i > 1 then 
-        False
-    else 
-        True
-
-neighbourg : Matrix.Matrix a ->(Int,Int)-> Matrix.Matrix a
-neighbourg m (x,y) =
-    m
-   |> Matrix.toList 
-   |> indexedFilter (around y)
-   |> List.map (indexedFilter (around x))
-   |> Matrix.fromList
- 
-sumCell : Matrix.Matrix Cell -> Int
-sumCell m = 
-     m
-     |> Matrix.toList
-     |> List.map (List.foldl foldlCell 0)
-     |> List.foldl (+) 0
-
-foldlCell : Cell -> Int -> Int
-foldlCell c s = 
-    case c.st of 
-        Dead -> 
-            s
-        Alive ->
-            s + 1
-          
-conwayCell :Matrix.Matrix Cell -> Cell -> Cell
-conwayCell  n c =
-    let 
-        sum = sumCell n
-    in
-        case c.st of 
-            Dead ->
-                if sum == 3 then
-                    { st = Alive }
-                else 
-                    { st = Dead }
-            Alive ->
-                if sum == 3 || sum == 4 then
-                    { st = Alive }
-                else 
-                    { st = Dead }
-
-                    
-
-applyConWayCellTo : Matrix.Matrix Cell-> (Int,Int) -> Cell -> Cell
-applyConWayCellTo m (x,y) c =
-    conwayCell (neighbourg m (x,y)) c   
-
-
-conway : Model -> Model
-conway model =
-    { model | matrix = Matrix.indexedMap (applyConWayCellTo model.matrix) model.matrix }
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Swap ( x, y ) ->
-            case model.mode of
-                Set ->
-                   {model | matrix = Matrix.set ( x, y ) (swap (Matrix.get ( x, y ) model.matrix)) model.matrix }
-                Update ->
-                    conway model
-        ChMode m ->
-            { model | mode = m }
-
-cellView : Int -> Int -> Cell -> Html Msg
-cellView y x c =
-    td
-        [ style "border" "1px black solid"
-        , style "width" "25px"
-        , style "height" "25px"
-        , style "background-color" (livingToColor c.st)
-        , onClick (Swap ( x, y ))
-        ]
-        []
-
-
-cellLineView : Int -> List Cell -> Html Msg
-cellLineView y ls =
-    tr [] (List.indexedMap (cellView y) ls)
-
-
-matrixView : Model -> Html Msg
-matrixView model =
-    table [] (List.indexedMap cellLineView (Matrix.toList model.matrix))
-
-view : Model -> Html Msg
-view model = 
-    div [] 
-        [ matrixView model
-        , button [onClick (ChMode Set)][text "Set"]
-        , button [onClick (ChMode Update)][text "Up"]
-        ]
-        
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = initialModel
-        , view = view
-        , update = update
-        }
